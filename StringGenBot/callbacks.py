12bbb0c6e77a954244de0267049f222b056ca2df
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-08-08 16:56:53 +0300

    Add files via upload

--- StringGenBot/callbacks.py
+++ StringGenBot/callbacks.py
@@ -32,9 +32,8 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
 
 
-ERROR_MESSAGE = (
-    "عـذرًا هناك خطأ \n\n**خطأ** : {} "
-    "\n\nأرجو مراسلة @A_D_P قم بتحويل هذه الرسالة له "
-    "أو يمكنك إعادة استخراج الجلسة"
-    "وشكرًا لاستخدام البوت المقدم من : @Tepthon"
-
+ERROR_MESSAGE = "ᴡᴛғ ! sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ. \n\n**ᴇʀʀᴏʀ** : {} " \
+            "\n\n**ᴩʟᴇᴀsᴇ ғᴏʀᴡᴀʀᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴛᴏ @A_D_P**, ɪғ ᴛʜɪs ᴍᴇssᴀɢᴇ " \
+            "ᴅᴏᴇsɴ'ᴛ ᴄᴏɴᴛᴀɪɴ ᴀɴʏ sᴇɴsɪᴛɪᴠᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ " \
+            "ʙᴇᴄᴀᴜsᴇ ᴛʜɪs ᴇʀʀᴏʀ ɪs **ɴᴏᴛ ʟᴏɢɢᴇᴅ ʙʏ ᴛʜᴇ ʙᴏᴛ** !"
+            
